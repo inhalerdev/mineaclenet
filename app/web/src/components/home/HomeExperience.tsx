b91d14b7a7a7a90b8f1ad961698f78a2848c862c
brandinsarazin <brandinsarazin@gmail.com>
2026-09-08 12:53:14 -0500

    9/8 1.67

--- app/web/src/components/home/HomeExperience.tsx
+++ app/web/src/components/home/HomeExperience.tsx
@@ -1,14 +1,8 @@
 import { VisitorHome } from "@/components/home/VisitorHome";
-import { AppSidebar } from "@/components/shell/AppSidebar";
 import { getCurrentViewer } from "@/features/auth/session";
 
 export async function HomeExperience() {
   const viewer = await getCurrentViewer();
 
-  return (
-    <div className="mineacle-app">
-      <AppSidebar viewer={viewer} />
-      <VisitorHome viewer={viewer} />
-    </div>
-  );
+  return <VisitorHome viewer={viewer} />;
 }
